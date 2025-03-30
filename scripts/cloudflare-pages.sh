3f9f152e9ee2d8619a6b0475edccc3e2e31b7818
Ricardo Iván Vieitez Parra <3857362+corrideat@users.noreply.github.com>
2025-03-30 16:34:03 +0000

    Misc non-source updates

--- scripts/cloudflare-pages.sh
+++ scripts/cloudflare-pages.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
 git fetch --tags
-curl -sSL  -o 'OpenJDK21U-jdk_x64_linux_hotspot_21.0.3_9.tar.gz' 'https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.3%2B9/OpenJDK21U-jdk_x64_linux_hotspot_21.0.3_9.tar.gz'
-printf 'fffa52c22d797b715a962e6c8d11ec7d79b90dd819b5bc51d62137ea4b22a340  OpenJDK21U-jdk_x64_linux_hotspot_21.0.3_9.tar.gz\n' | sha256sum -c
-tar -xzf 'OpenJDK21U-jdk_x64_linux_hotspot_21.0.3_9.tar.gz' 
-export JAVA_HOME="$(pwd)/jdk-21.0.3+9"
+curl -sSL  -o 'OpenJDK21U-jdk_x64_linux_hotspot_21.0.6_7.tar.gz' 'https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.6%2B7/OpenJDK21U-jdk_x64_linux_hotspot_21.0.6_7.tar.gz'
+printf 'a2650fba422283fbed20d936ce5d2a52906a5414ec17b2f7676dddb87201dbae  OpenJDK21U-jdk_x64_linux_hotspot_21.0.6_7.tar.gz\n' | sha256sum -c
+tar -xzf 'OpenJDK21U-jdk_x64_linux_hotspot_21.0.6_7.tar.gz'
+export JAVA_HOME="$(pwd)/jdk-21.0.6+7"
 export PATH="$JAVA_HOME/bin:$PATH"
 export LC_ALL="C"
 export TZ="UTC"
